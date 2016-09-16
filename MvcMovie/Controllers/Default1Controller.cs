c1f2283d0bf6b82f3fd14c52d375285bb93142b5
anurag agar <prateekshivhare8@gmail.com>
2016-09-16 17:50:40 +0530

    sample commit

---
+++ MvcMovie/Controllers/Default1Controller.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{ 
+    public class Default1Controller : Controller
+    {
+        private MovieDbContext db = new MovieDbContext();
+
+        //
+        // GET: /Default1/
+
+        public ViewResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(movie);
+        }
+        
+        //
+        // GET: /Default1/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        //
+        // GET: /Default1/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMovie/MovieDBContext__MvcMovie/Movie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+ 
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime ReleaseDate { get; set; }
+
+       
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Price Required")]
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+
+
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public string Project { get; set; }
+        public string Skill { get; set; }
+
+
+
+        
+    }
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
